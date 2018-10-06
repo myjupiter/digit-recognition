a3903f2e8364a0d28433acd5ef3b817ec1e1187e
myjupiter <paisal@ifz.co.th>
2018-10-06 13:04:23 +0700

    update

--- performRecognition.py
+++ performRecognition.py
@@ -33,27 +33,27 @@ ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_
 # Get rectangles contains each contour
 rects = [cv2.boundingRect(ctr) for ctr in ctrs]
 
-print(rects)
-pprint(rects)
+# print(rects)
+# pprint(rects)
 
 # For each rectangular region, calculate HOG features and predict
 # the digit using Linear SVM.
-# for rect in rects:
-#     # Draw the rectangles
-#     cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
-#     # Make the rectangular region around the digit
-#     leng = int(rect[3] * 1.6)
-#     pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
-#     pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
-#     roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
-#     # Resize the image
-#     roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
-#     roi = cv2.dilate(roi, (3, 3))
-#     # Calculate the HOG features
-#     roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
-#     roi_hog_fd = pp.transform(np.array([roi_hog_fd], 'float64'))
-#     nbr = clf.predict(roi_hog_fd)
-#     cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+for rect in rects:
+    # Draw the rectangles
+    # cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
+    # Make the rectangular region around the digit
+    leng = int(rect[3] * 1.6)
+    pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
+    pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
+    roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
+    # # Resize the image
+    # roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+    # roi = cv2.dilate(roi, (3, 3))
+    # # Calculate the HOG features
+    # roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+    # roi_hog_fd = pp.transform(np.array([roi_hog_fd], 'float64'))
+    # nbr = clf.predict(roi_hog_fd)
+    # cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
 
 # cv2.namedWindow("Resulting Image with Rectangular ROIs", cv2.WINDOW_NORMAL)
 # cv2.imshow("Resulting Image with Rectangular ROIs", im)
