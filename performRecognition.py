5f2152012dee5f8e8525173382e87f66ebf2394a
myjupiter <paisal@ifz.co.th>
2018-10-06 13:06:17 +0700

    update

--- performRecognition.py
+++ performRecognition.py
@@ -49,11 +49,12 @@ for rect in rects:
     # # Resize the image
     roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
     roi = cv2.dilate(roi, (3, 3))
-    print "roi: %s" % roi
+    # print "roi: %s" % roi
     # # Calculate the HOG features
-    # roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
-    # roi_hog_fd = pp.transform(np.array([roi_hog_fd], 'float64'))
-    # nbr = clf.predict(roi_hog_fd)
+    roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+    roi_hog_fd = pp.transform(np.array([roi_hog_fd], 'float64'))
+    nbr = clf.predict(roi_hog_fd)
+    print nbr
     # cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
 
 # cv2.namedWindow("Resulting Image with Rectangular ROIs", cv2.WINDOW_NORMAL)
